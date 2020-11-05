9283f6cc99fe0c62a02ba689198109532e99b265
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-05 13:07:35 -0800

    Added panels and allowed to swap between views

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,65 @@
 import React, { Component } from "react";
-
+import Loading from "components/Loading.js";
+import Panel from "components/Panel.js";
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
-    return <main className={dashboardClasses} />;
+class Dashboard extends Component {
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+  }));
+  }
+  state = {
+    loading: false,
+    focused: null
+  }
+  
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      />
+    ))
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
